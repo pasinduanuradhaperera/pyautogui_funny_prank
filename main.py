9b3e0dd41d5480d8115947aa2009f0c599d0c84d
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2023-03-18 12:21:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,25 @@
 import pyautogui as gui
 import time
 
-time.sleep(30)
+time.sleep(10)
 
 # Go to Desktop
 gui.keyDown("win")
 gui.press("d")
 gui.keyUp("win")
 
-# open start menue 
+# open start menue
 gui.press("win")
 
+time.sleep(2)
 # search notepad
-gui.write("note pad")
+gui.write("notepad")
+gui.press("enter")
 
-# wait to open 
+# wait to open
 time.sleep(10)
 
-# type you messege 
+# type you messege
 for i in range(100):
-  gui.write("your messege here")
-  gui.press("enter")
-  
+    gui.write("your messege here")
+    gui.press("enter")
